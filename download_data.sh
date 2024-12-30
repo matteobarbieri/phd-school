4d285fd3556a11bd5b067c3e12ebd92a3b2d3625
Matteo Barbieri <neopard@gmail.com>
2024-12-30 20:25:08 +0100

    Move downloaded fonts in home/fonts

--- download_data.sh
+++ download_data.sh
@@ -8,8 +8,8 @@ wget https://www.dropbox.com/s/t0i4x9bgaml3nra/resnet101_allfonts_mnist.pth?dl=1
     -O models/resnet101_allfonts_mnist.pth
 
 # Create folder for fonts
-mkdir -p fonts
+mkdir -p home/fonts
 
 # Download font from dropbox
 wget https://www.dropbox.com/s/wad32grfxvhtamp/Helvetica-Bold-Font.ttf?dl=1 \
-    -O fonts/Helvetica-Bold-Font.ttf
+    -O home/fonts/Helvetica-Bold-Font.ttf
