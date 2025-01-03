e977ec884b2998dd0e253a6d83e9f252ea95d717
Matteo Barbieri <matteo.barbieri@epidemicsound.com>
2025-01-03 20:05:58 +0100

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 # Create folder if it does not exist
-mkdir -p models
+mkdir -p home/models
 
 # Download model from dropbox
-wget https://www.dropbox.com/s/t0i4x9bgaml3nra/resnet101_allfonts_mnist.pth?dl=1 \
-    -O home/models/resnet101_allfonts_mnist.pth
+wget \
+    -O home/models/best_candidate.ckpt \
+    "https://www.dropbox.com/scl/fi/272x8cxlsevkpnn40mgnk/best_candidate.ckpt?rlkey=uokxm82dzm825xj58ry36n5ux&st=bxzkpftu&dl=1" \ 
+    
 
 # Create folder for fonts
 mkdir -p home/fonts
 
 # Download font from dropbox
-wget https://www.dropbox.com/s/wad32grfxvhtamp/Helvetica-Bold-Font.ttf?dl=1 \
-    -O home/fonts/Helvetica-Bold-Font.ttf
+wget -O home/fonts/Helvetica-Bold-Font.ttf \
+    "https://www.dropbox.com/s/wad32grfxvhtamp/Helvetica-Bold-Font.ttf?dl=1" \
+    
