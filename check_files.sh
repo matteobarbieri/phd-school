86b3b0ce6bbac088a240912143bc56f0132743a7
Matteo Barbieri <matteo.barbieri@epidemicsound.com>
2025-01-13 15:43:47 +0100

    Add script to check downloaded files

---
+++ check_files.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FILES_LIST="home/models/best_candidate.ckpt home/fonts/Helvetica-Bold-Font.ttf home/data/MNIST_CSV/mnist_test.csv home/data/MNIST_CSV/mnist_train.csv"
+
+for f in $FILES_LIST; do
+    if [ -f $f ]; then
+        echo "✅ $f exists"
+    else
+        echo "❌ !!! $f DOES NOT EXIST"
+    fi
+done
