933fbe3e4430f4545cd9f7789b7b8135b1cc598b
Matteo Barbieri <matteo.barbieri@epidemicsound.com>
2025-01-16 20:08:02 +0100

    Update README

--- check_files.sh
+++ check_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FILES_LIST="home/models/best_candidate.ckpt home/fonts/Helvetica-Bold-Font.ttf home/data/MNIST_CSV/mnist_test.csv home/data/MNIST_CSV/mnist_train.csv"
+FILES_LIST="home/models/best_candidate.ckpt home/fonts/Helvetica-Bold-Font.ttf home/data/MNIST_CSV/mnist_test.csv home/data/MNIST_CSV/mnist_train.csv home/data/challenge/challenge_train.csv home/data/challenge/challenge_test.csv"
 
 for f in $FILES_LIST; do
     if [ -f $f ]; then
--- download_data.sh
+++ download_data.sh
@@ -17,10 +17,19 @@ wget -O home/fonts/Helvetica-Bold-Font.ttf \
     "https://www.dropbox.com/s/wad32grfxvhtamp/Helvetica-Bold-Font.ttf?dl=1" \
     
 
-# Create home for MNIST.csv
+# Create folder for MNIST.csv
 mkdir -p home/data
 
 wget -O home/data/MNIST_CSV.zip \
 "https://www.dropbox.com/scl/fi/n6i5j75j5rgjez27qpw0n/MNIST_CSV.zip?rlkey=vc253rogq2gxdqearqrhv1ytf&dl=0"
 
 unzip home/data/MNIST_CSV.zip -d home/data/MNIST_CSV
+
+# Create folder challenge data
+mkdir -p home/data/challenge
+
+wget -O home/data/challenge/challenge_train.csv \
+"https://www.dropbox.com/scl/fi/shp2qvk6yoia5wyp21gev/challenge_train.csv?rlkey=5v4yt1nj2fca5hqafhiu22444&st=pciyaa5b&dl=0"
+
+wget -O home/data/challenge/challenge_test.csv \
+"https://www.dropbox.com/scl/fi/gszitamicqvjv42esbgvu/challenge_test.csv?rlkey=0p20v2lpext07g5jqe6miamwf&st=pdaqaclz&dl=0"
